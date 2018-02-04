adec96aa4d43394bf7b5a690fe7d22beddc6536a
Javier Mino <javier.svg@gmail.com>
2018-02-04 17:45:19 -0300

    add search criteria for heroes

--- src/main/java/com/javiersvg/tourofheroes/HeroRepository.java
+++ src/main/java/com/javiersvg/tourofheroes/HeroRepository.java
@@ -1,9 +1,14 @@
 package com.javiersvg.tourofheroes;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Created by Javier on 1/10/2018.
  */
 public interface HeroRepository extends MongoRepository<Hero, String> {
+
+    List<Hero> findByNameLike(@Param("name") String name);
 }

d4ced2a0448ce2d0e31ad73d1b293fc63e16f71c
Javier Mino <javier.svg@gmail.com>
2019-05-02 13:51:07 -0500

    solve spring request code smell

--- src/main/java/com/javiersvg/tourofheroes/UserController.java
+++ src/main/java/com/javiersvg/tourofheroes/UserController.java
@@ -4,8 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +13,7 @@ public class UserController {
     @Autowired
     private AppUserRepository repository;
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @GetMapping("/user")
     public User getUser(Authentication authentication) {
         return repository.findOne(Example.of(new User((Jwt) authentication.getPrincipal()))).orElseThrow();
     }
